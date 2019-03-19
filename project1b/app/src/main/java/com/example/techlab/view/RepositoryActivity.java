28aaba3714029ae161af713587d47c202d65c85f
Sopa <tensopa@gmail.com>
2019-03-19 21:51:36 +0100

    Revert "Revert "renamed home activity to repository""
    This reverts commit 05daa2956310ee723d34ea629a09cb553f1f340b.

--- project1b/app/src/main/java/com/example/techlab/view/RepositoryActivity.java
+++ project1b/app/src/main/java/com/example/techlab/view/RepositoryActivity.java
@@ -3,19 +3,18 @@ package com.example.techlab.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import com.example.techlab.R;
 import com.example.techlab.model.Users;
 
-public class HomePageActivity extends AppCompatActivity {
+public class RepositoryActivity extends AppCompatActivity {
     TextView username;
     TextView welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_repository);
         Intent intent = getIntent();
         Users activeUser = intent.getParcelableExtra("activeUser");
         username = (TextView) findViewById(R.id.userNameHomePage);
