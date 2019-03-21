b4f22dc737eca976f487cc127db211f5b165d1f7
Sopa <tensopa@gmail.com>
2019-03-22 00:43:13 +0100

    applied data binding to recyclerview

---
+++ project1b/app/src/main/java/com/example/techlab/adapter/ProductAdapter.java
@@ -0,0 +1,58 @@
+package com.example.techlab.adapter;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.example.techlab.R;
+import com.example.techlab.databinding.ActivityProductItemBinding;
+import com.example.techlab.model.Electronics;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+
+    private List<Electronics> electronics;
+    private Context context;
+
+    public ProductAdapter(List<Electronics> electronics, Context context) {
+        this.electronics = electronics;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ActivityProductItemBinding binding = DataBindingUtil
+                .inflate(LayoutInflater.from(viewGroup.getContext())
+                , R.layout.activity_product_item
+                ,viewGroup
+                ,false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Electronics electronic = electronics.get(i);
+        viewHolder.productItemBinding.setElectronicsItem(electronic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return electronics.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        // Binding variables
+        public ActivityProductItemBinding productItemBinding;
+
+        public ViewHolder(ActivityProductItemBinding productLayoutBinding) {
+            super(productLayoutBinding.getRoot());
+            productItemBinding = productLayoutBinding;
+
+        }
+    }
+}
--- project1b/app/src/main/java/com/example/techlab/view/ProductManagementActivity.java
+++ project1b/app/src/main/java/com/example/techlab/view/ProductManagementActivity.java
@@ -1,15 +1,54 @@
 package com.example.techlab.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.example.techlab.R;
+import com.example.techlab.adapter.ProductAdapter;
+import com.example.techlab.databinding.ActivityProductBeheerBinding;
+import com.example.techlab.model.Electronics;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductManagementActivity extends AppCompatActivity {
-
+    private ActivityProductBeheerBinding binding;
+    private ProductAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_beheer);
+        binding = DataBindingUtil.setContentView(this,
+                R.layout.activity_product_beheer);
+        adapter = new ProductAdapter(getProductData(),this);
+
+        binding.electronicsListItems.setAdapter(adapter);
+        binding.electronicsListItems.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    private List<Electronics> getProductData() {
+        List<Electronics> electronics = new ArrayList<>();
+
+        Electronics electronic = new Electronics(
+                "124"
+                ,"Sony"
+                ,"playstation"
+                ,50
+                , 0
+                ,"game"
+                );
+        electronics.add(electronic);
+        electronic = new Electronics(
+                "124"
+                ,"Sony"
+                ,"playstation"
+                ,50
+                , 0
+                ,"game"
+        );
+        electronics.add(electronic);
+        return electronics;
     }
 }
