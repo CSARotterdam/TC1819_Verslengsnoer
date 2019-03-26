9b8ff499c9363c10101d5a8ece7d62120029a39a
Sopa <tensopa@gmail.com>
2019-03-27 00:16:35 +0100

    :)

---
+++ project1b/app/src/main/java/com/example/techlab/view/ProductManagementActivity.java
@@ -0,0 +1,89 @@
+package com.example.techlab.view;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+
+import com.example.techlab.R;
+import com.example.techlab.adapter.ProductAdapter;
+import com.example.techlab.databinding.ActivityProductBeheerBinding;
+import com.example.techlab.db.DataSource;
+import com.example.techlab.model.Electronics;
+import com.example.techlab.model.Users;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductManagementActivity extends AppCompatActivity {
+
+    DataSource dataSource;
+    private ActivityProductBeheerBinding binding;
+    private ProductAdapter adapter;
+    private Users activeUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,
+                R.layout.activity_product_beheer);
+        dataSource = new DataSource(this);
+        Intent intent = getIntent();
+        activeUser = intent.getParcelableExtra("activeUser");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        dataSource.open();
+
+        Cursor cursor = dataSource.selectAllproduct();
+        ArrayList<Electronics> productData = getProductData(cursor);
+        adapter = new ProductAdapter(productData,this, activeUser);
+
+        binding.electronicsListItems.setAdapter(adapter);
+        binding.electronicsListItems.setLayoutManager(new LinearLayoutManager(this));
+        binding.electronicsListItems.setNestedScrollingEnabled(false);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        dataSource.close();
+    }
+
+    public void addProductPageButton(View view){
+        Intent startNewActivity = new Intent(this, AddProductActivity.class);
+        startNewActivity.putExtra("activeUser",activeUser);
+        startActivity(startNewActivity);
+    }
+
+    protected ArrayList<Electronics> getProductData(Cursor cursor) {
+
+        ArrayList<Electronics> electronicsList = new ArrayList<>();
+
+        cursor.moveToFirst();
+        for (int i = cursor.getCount(); i > 0; i--) {
+            electronicsList.add(new Electronics(
+                    cursor.getString(0)
+                    , cursor.getString(1)
+                    , cursor.getString(2)
+                    ,cursor.getInt(3)
+                    ,cursor.getInt(4)
+                    ,cursor.getString(5)
+                    ,cursor.getString(6)));
+            if (i>1){
+                cursor.moveToNext();
+            }
+        }
+        return electronicsList;
+    }
+    @Override
+    public void onBackPressed() {
+        finish();
+        Intent startNewActivity = new Intent(this, RepositoryActivity.class);
+        startNewActivity.putExtra("activeUser",activeUser);
+        startActivity(startNewActivity);
+    }
+}
---
+++ project1b/app/src/main/java/com/example/techlab/view/SignUpActivity.java
@@ -0,0 +1,63 @@
+package com.example.techlab.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.techlab.R;
+import com.example.techlab.model.Users;
+import com.example.techlab.db.DataSource;
+
+public class SignUpActivity extends AppCompatActivity {
+    EditText userFirstNameInput;
+    EditText userSurnameInput;
+    EditText schoolEmailInput;
+    EditText passwordInput;
+    DataSource dataSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        userFirstNameInput = (EditText) findViewById(R.id.firstNameInput);
+        userSurnameInput = (EditText) findViewById(R.id.surnameInput);
+        schoolEmailInput = (EditText) findViewById(R.id.schoolEmailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        dataSource = new DataSource(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        dataSource.open();
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        dataSource.close();
+    }
+
+    public void signUpButtonClicked(View view){
+        // creating new user instance
+        Users user = new Users(
+                userFirstNameInput.getText().toString(),
+                userSurnameInput.getText().toString(),
+                schoolEmailInput.getText().toString(),
+                passwordInput.getText().toString()
+                ,0,
+                "student");
+        // insert new user
+        dataSource.insertUser(user);
+        // reset form input text field
+        userFirstNameInput.setText("");
+        userSurnameInput.setText("");
+        schoolEmailInput.setText("");
+        passwordInput.setText("");
+    }
+
+
+
+
+}
