812b6b0fe5a7a1df16a7781a900a8d03dd040155
Sopa <tensopa@gmail.com>
2019-03-28 22:39:52 +0100

    product update screen product name bug fixed

--- project1b/app/src/main/java/com/example/techlab/db/DataSource.java
+++ project1b/app/src/main/java/com/example/techlab/db/DataSource.java
@@ -132,7 +132,7 @@ public class DataSource {
                 + mTechLabDataBaseHelper.COLUMN_ID + " = " + id + ";";
         Cursor cursor = mDatabase.rawQuery(checkQuery,null);
         cursor.moveToFirst();
-        Electronics product = new Electronics(cursor.getString(1),cursor.getString(2),cursor.getString(3)
+        Electronics product = new Electronics(cursor.getString(1),cursor.getString(3),cursor.getString(2)
                 ,cursor.getInt(4),cursor.getInt(5),cursor.getString(6),cursor.getString(7));
         return product;
     }
