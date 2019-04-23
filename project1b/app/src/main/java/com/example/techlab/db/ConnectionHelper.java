9ec46866e2f5a4bc26b7bf92793976dc64daae51
tensopa <tensopa@gmail.com>
2019-04-23 18:03:22 +0200

    jtds.jar file downloaded

---
+++ project1b/app/src/main/java/com/example/techlab/db/ConnectionHelper.java
@@ -0,0 +1,25 @@
+package com.example.techlab.db;
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+
+import java.sql.Connection;
+
+public class ConnectionHelper {
+ /*   String IP,DB,UserName,Password;
+    @SuppressLint("NewApi")
+    public Connection connection(){
+        IP = "";
+        DB = "";
+        UserName = "";
+        Password = "";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        java.sql.Connection connection = null;
+        String ConnectionURL = null;
+        try{
+            Class.forName("")
+        }
+    }*/
+
+}
