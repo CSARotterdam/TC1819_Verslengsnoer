1116e18fbaba9ef5dda1ec0476cb75dd0b4d5e2e
tensopa <tensopa@gmail.com>
2019-04-24 15:30:58 +0200

    Revert "test"
    This reverts commit 2ca6c03c95d7b294e8611f87966afa7c0de22486.

--- project1b/app/src/main/java/com/example/techlab/view/Product_AddProductActivity.java
+++ project1b/app/src/main/java/com/example/techlab/view/Product_AddProductActivity.java
@@ -16,7 +16,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.techlab.R;
-import com.example.techlab.db.DataManagement;
 import com.example.techlab.db.DataSource;
 import com.example.techlab.db.imageConverter;
 import com.example.techlab.model.Electronics;
@@ -33,7 +32,6 @@ public class Product_AddProductActivity extends AppCompatActivity {
     private DataSource dataSource;
     private ImageView productUploadimageView;
     private Bitmap image;
-    DataManagement dataManagement;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +44,6 @@ public class Product_AddProductActivity extends AppCompatActivity {
         productCategory = findViewById(R.id.productCategoryTextInput);
         productDescription = findViewById(R.id.productDescriptionTextInput);
         productUploadimageView = findViewById(R.id.productUploadimageView);
-        dataManagement = new DataManagement();
 
         dataSource = new DataSource(this);
         Intent intent = getIntent();
@@ -82,18 +79,16 @@ public class Product_AddProductActivity extends AppCompatActivity {
         Electronics newProduct = new Electronics(
                 productId.getText().toString(),
                 productManufacturer.getText().toString(),
-                productCategory.getText().toString(),
                 productName.getText().toString(),
                 Integer.parseInt(productStock.getText().toString()),
                 0,
+                productCategory.getText().toString(),
                 productDescription.getText().toString())
                 ;
         image = ((BitmapDrawable)productUploadimageView.getDrawable()).getBitmap();
         int width = image.getWidth()/4;
         int height = image.getHeight()/4;
-        dataManagement.addProductData(productId.getText().toString(),productManufacturer.getText().toString()
-                ,productCategory.getText().toString(),productName.getText().toString(),Integer.parseInt(productStock.getText().toString())
-                ,0,imageConverter.getByte(image));
+        insertData(newProduct,Bitmap.createScaledBitmap(image, width, height, false));
 
 
         // reset form input text field
@@ -105,6 +100,10 @@ public class Product_AddProductActivity extends AppCompatActivity {
         productDescription.setText("");
         Intent intent = new Intent(this, Product_ProductManagementActivity.class);
         startActivity(intent);
+    }
+    private void insertData(Electronics newProduct,Bitmap image){
+        // insert new product
+        dataSource.insertProduct(newProduct,imageConverter.getByte(image));
     }
 
     @Override
