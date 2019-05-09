c9a1350b48ef80196d5f17b4ff7552b07a5a9407
Saloua <saloua1998@live.nl>
2019-05-09 10:54:22 +0200

    voorwaarden in html
    Hierdoor kon ik de tekst duidelijker maken. bijvoorbeeld door titels dikgedrukt te maken

--- project1b/app/src/main/java/com/example/techlab/view/Product_Voorwaarden.java
+++ project1b/app/src/main/java/com/example/techlab/view/Product_Voorwaarden.java
@@ -2,6 +2,9 @@ package com.example.techlab.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
 import com.example.techlab.R;
 
 public class Product_Voorwaarden extends AppCompatActivity{
@@ -9,5 +12,9 @@ public class Product_Voorwaarden extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_voorwaarden);
+
+//        This makes an html textview
+        TextView voorwaarden = (TextView) findViewById(R.id.Voorwaarden);
+        voorwaarden.setText(Html.fromHtml(getString(R.string.gebruikersvoorwaarden)));
     }
 }
