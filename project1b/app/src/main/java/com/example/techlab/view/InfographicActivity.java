8ba99a2e9a86ff0c6244b1fcd16e4d9ec1aafb9c
Sopa <tensopa@gmail.com>
2019-05-31 14:34:48 +0200

    infographic value formatere added

--- project1b/app/src/main/java/com/example/techlab/view/InfographicActivity.java
+++ project1b/app/src/main/java/com/example/techlab/view/InfographicActivity.java
@@ -12,6 +12,7 @@ import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -25,6 +26,13 @@ public class InfographicActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infographic);
         dataManagementInfographic = new DataManagementInfographic();
+
+        ValueFormatter formatter = new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return ((int) value) + " ";
+            }
+        };
 
 
 
@@ -43,6 +51,7 @@ public class InfographicActivity extends AppCompatActivity {
         PieData data = new PieData((pieDataSet1));
         data.setValueTextSize(10f);
         data.setValueTextColor(Color.WHITE);
+        data.setValueFormatter(formatter);
         pieChart.setData(data);
 
         pieChart2 = findViewById(R.id.mostPopularUserChart);
@@ -59,6 +68,7 @@ public class InfographicActivity extends AppCompatActivity {
         PieData data2 = new PieData((pieDataSet2));
         data2.setValueTextSize(10f);
         data2.setValueTextColor(Color.WHITE);
+        data2.setValueFormatter(formatter);
         pieChart2.setData(data2);
 
 
@@ -75,5 +85,6 @@ public class InfographicActivity extends AppCompatActivity {
         Intent startNewActivity = new Intent(this,  Product_InventoryActivity.class);
         startActivity(startNewActivity);
     }
+
 
 }
