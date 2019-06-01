aef7731dcccba7d4f84baacf89af097fb0696bdc
Guan Hao Wu <guanhao_wu@hotmail.com>
2019-06-01 18:48:49 +0200

    Update User_management_user_InfoActivity.java
    removed unused import

--- project1b/app/src/main/java/com/example/techlab/view/User_management_user_InfoActivity.java
+++ project1b/app/src/main/java/com/example/techlab/view/User_management_user_InfoActivity.java
@@ -8,7 +8,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
