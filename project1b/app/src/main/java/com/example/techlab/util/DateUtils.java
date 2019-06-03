3258d94ef1380680e2e7c8964adc66104b105c8e
Sopa <tensopa@gmail.com>
2019-06-03 20:37:48 +0200

    time issue fixed

--- project1b/app/src/main/java/com/example/techlab/util/DateUtils.java
+++ project1b/app/src/main/java/com/example/techlab/util/DateUtils.java
@@ -3,7 +3,6 @@ package com.example.techlab.util;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.TimeZone;
 
 public class DateUtils {
     public static final String DATE_FORMAT = "dd-MM-yyyy HH:mm";
@@ -16,7 +15,6 @@ public class DateUtils {
 
     public static String getCurrentDate(Timestamp date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         return dateFormat.format(date);
     }
 
